b3cdf6cb134b692a9d2f39d5b86376f4a6296b6c
Ruslan <RMingalievF@icloud.com>
2023-06-27 08:41:37 +0300

    Initial Commit

---
+++ Mortage_Optim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mortage_Optim
+//
+//  Created by Руслан Мингалиев on 27.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
