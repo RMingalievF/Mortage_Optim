94822d1f55e54c76566d487846862fdfd7cf4767
Ruslan <RMingalievF@icloud.com>
2023-07-01 22:26:24 +0300

    Add private func calculateListMonthlyPayment()

--- Mortage_Optim/Models/CalculationMortage.swift
+++ Mortage_Optim/Models/CalculationMortage.swift
@@ -5,39 +5,87 @@
 //  Created by Руслан Мингалиев on 28.06.2023.
 //ОБЩАЯ_СТАВКА = (1 + ЕЖЕМЕСЯЧНАЯ_СТАВКА) ^ СРОК_ИПОТЕКИ_МЕСЯЦЕВ
 // ЕЖЕМЕСЯЧНЫЙ_ПЛАТЕЖ = СУММА_КРЕДИТА * ЕЖЕМЕСЯЧНАЯ_СТАВКА * ОБЩАЯ_СТАВКА / (ОБЩАЯ_СТАВКА - 1)
-//
+
+//ПРОЦЕНТНАЯ_ЧАСТЬ = ОСТАТОК_ДОЛГА * ЕЖЕМЕСЯЧНАЯ_СТАВКА
+
+//ОСНОВНАЯ_ЧАСТЬ = ЕЖЕМЕСЯЧНЫЙ_ПЛАТЕЖ - ПРОЦЕНТНАЯ_ЧАСТЬ
 
 class Mortgage {
     
+    
     let mortgageDebit: Float // Сумма ипотеки
-    let mortgageTermMonths: Int // Срок ипотеки в днях
+    //var mortgageBalance: Float // Остаток по ипотеке
+    
+    let mortgageTermMonths: Int // Срок ипотеки в месяцах
     let interestRate: Float  // Процентная ставка
     
-    //Общая ставка
+    var listMonthlyPayment: [Int: [Float]] {
+        calculateListMonthlyPayment()
+    }
+    
+    //ОБЩАЯ_СТАВКА = (1 + ЕЖЕМЕСЯЧНАЯ_СТАВКА) ^ СРОК_ИПОТЕКИ_МЕСЯЦЕВ
     var totalRate: Float {
         calculateTotalRate ()
-        }
-    private var monthlyinterestRate: Float{
+    }
+    //ЕЖЕМЕСЯЧНАЯ_СТАВКА
+    var monthlyinterestRate: Float{
         interestRate/12/100
     }
-    // Ежемесячный платеж
+    //ЕЖЕМЕСЯЧНЫЙ_ПЛАТЕЖ = СУММА_КРЕДИТА * ЕЖЕМЕСЯЧНАЯ_СТАВКА * ОБЩАЯ_СТАВКА / (ОБЩАЯ_СТАВКА - 1)
     var monthlyPayment: Float{
         mortgageDebit * monthlyinterestRate * totalRate / (totalRate - 1.0)
+    }
+    //ПРОЦЕНТНАЯ_ЧАСТЬ = ОСТАТОК_ДОЛГА * ЕЖЕМЕСЯЧНАЯ_СТАВКА
+    var percentagePart: Float {
+        mortgageDebit * monthlyinterestRate
+    }
+    //ОСНОВНАЯ_ЧАСТЬ = ЕЖЕМЕСЯЧНЫЙ_ПЛАТЕЖ - ПРОЦЕНТНАЯ_ЧАСТЬ
+    var mainPart: Float {
+        monthlyPayment - percentagePart
+    }
+    //ПЕРЕПЛАТА = ЕЖЕМЕСЯЧНЫЙ_ПЛАТЕЖ * СРОК_ИПОТЕКИ_МЕСЯЦЕВ - СУММА_КРЕДИТА
+    var overpayment: Float{
+        monthlyPayment * Float(mortgageTermMonths) - mortgageDebit
     }
     
     init(mortgageDebit: Float, mortgageTermMonths: Int, interestRate: Float) {
         self.mortgageDebit = mortgageDebit
         self.mortgageTermMonths = mortgageTermMonths
         self.interestRate = interestRate
-            //self.monthlyPayment = monthlyPayment
+        
+    }
+    //ОБЩАЯ_СТАВКА = (1 + ЕЖЕМЕСЯЧНАЯ_СТАВКА) ^ СРОК_ИПОТЕКИ_МЕСЯЦЕВ
+    
+     func calculateTotalRate () -> Float{
+         var total: Float = 1
+        for _ in 1...mortgageTermMonths {
+            total *= (1.0 + monthlyinterestRate)
+        }
+         return total
     }
     
-   private func calculateTotalRate () -> Float{
-        var total: Float = 0.0
-            for _ in 1...mortgageTermMonths {
-                total *= (1.0 + monthlyinterestRate)
-            }
-    return total
-    }
+    //ПРОЦЕНТНАЯ_ЧАСТЬ = 1 500 000 руб * 0.008 = 12 000 руб
+   // ОСНОВНАЯ_ЧАСТЬ = 14 080 руб - 12 000 руб = 2 080 руб
     
+   private func calculateListMonthlyPayment() -> [Int: [Float]]{
+        var listPayment = [Int: [Float]]()
+        let listPaymentSorted =  [Int: [Float]]()
+       
+        var mortgageBalance = mortgageDebit
+        for numberPayment in 1...mortgageTermMonths {
+            var percentPart = (mortgageBalance * monthlyinterestRate)
+            percentPart.round(.toNearestOrAwayFromZero)
+            var mainPart = monthlyPayment - percentPart
+            mainPart.round(.toNearestOrAwayFromZero)
+            var recult = percentPart + mainPart
+            
+            listPayment[numberPayment] = [mortgageBalance, percentPart, mainPart, recult]
+            
+            print(listPayment)
+            mortgageBalance = mortgageBalance - mainPart
+            
+            
+        }
+return listPayment
+    }
 }
