7871a5495c65fe0ff2d8506c488c7b6fd58553ef
Ruslan <RMingalievF@icloud.com>
2023-07-26 22:36:47 +0500

    Add login VC

---
+++ Mortage_Optim/ViewControllers/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  Mortage_Optim
+//
+//  Created by Руслан Мингалиев on 26.07.2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
